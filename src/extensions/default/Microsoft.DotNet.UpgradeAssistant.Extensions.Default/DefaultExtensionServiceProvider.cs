e748b55e56b8982bd8a47b2fb29866c219a795a3
Taylor Southwick <tasou@microsoft.com>
2022-08-10 17:49:23 -0700

    REMOVE: Remove analyzers

--- src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Extensions.Default/DefaultExtensionServiceProvider.cs
+++ src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Extensions.Default/DefaultExtensionServiceProvider.cs
@@ -5,8 +5,6 @@ using System;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.DotNet.UpgradeAssistant.Dependencies;
-using Microsoft.DotNet.UpgradeAssistant.Extensions.Default.Analyzers;
-using Microsoft.DotNet.UpgradeAssistant.Extensions.Default.CodeFixes;
 using Microsoft.DotNet.UpgradeAssistant.Steps.Packages.Analyzers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,7 +21,6 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Default
             }
 
             AddUpgradeSteps(services);
-            AddAnalyzersAndCodeFixProviders(services.Services);
             AddPackageReferenceAnalyzers(services.Services);
         }
 
@@ -35,34 +32,6 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Default
             services.AddSolutionSteps();
             services.AddSourceUpdaterStep();
             services.AddTemplateInserterStep();
-        }
-
-        // This extension only adds default analyzers and code fix providers, but other extensions
-        // can register additional analyzers and code fix providers, as needed.
-        private static void AddAnalyzersAndCodeFixProviders(IServiceCollection services)
-        {
-            // Add source analyzers and code fix providers (note that order doesn't matter as they're run alphabetically)
-            // Analyzers
-            services.AddTransient<DiagnosticAnalyzer, ApiAlertAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, WinUIApiAlertAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, AttributeUpgradeAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, BinaryFormatterUnsafeDeserializeAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, HtmlHelperAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, HttpContextCurrentAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, HttpContextIsDebuggingEnabledAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, TypeUpgradeAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, UrlHelperAnalyzer>();
-            services.AddTransient<DiagnosticAnalyzer, UsingSystemWebAnalyzer>();
-
-            // Code fix providers
-            services.AddTransient<CodeFixProvider, AttributeUpgradeCodeFixer>();
-            services.AddTransient<CodeFixProvider, BinaryFormatterUnsafeDeserializeCodeFixer>();
-            services.AddTransient<CodeFixProvider, HtmlHelperCodeFixer>();
-            services.AddTransient<CodeFixProvider, HttpContextCurrentCodeFixer>();
-            services.AddTransient<CodeFixProvider, HttpContextIsDebuggingEnabledCodeFixer>();
-            services.AddTransient<CodeFixProvider, TypeUpgradeCodeFixer>();
-            services.AddTransient<CodeFixProvider, UrlHelperCodeFixer>();
-            services.AddTransient<CodeFixProvider, UsingSystemWebCodeFixer>();
         }
 
         // This extension only adds default package reference analyzers, but other extensions
